f08ef56dda6bfd7f6d4b6a68b69b9cc5a7906804
nic-ball <nicholasball147@gmail.com>
2020-11-14 14:16:45 +0000

    Added GraphQL queries and mutations for all objects

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,79 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
 module.exports = {
   Query: {
-    sayHi: () => "HI!!!"
+    getUsers: async () => {
+      try {
+        const users = await User.findAll();
+        return users;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    getUser: async (_, { id }) => {
+      try {
+        const user = await User.findOne({ where: { id } });
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  Mutation: {
+    createUser: async (_, args) => {
+      let { username, email, password } = args;
+      try {
+        // 1. Check if a user exists in DB
+        const getUser = await User.findOne({ where: { email: email } });
+        if (!getUser) {
+          // 2. Hash user password
+          password = await bcrypt.hash(password, 12);
+          // 3. Store the user in DB
+          const user = await User.create({
+            username,
+            email,
+            password
+          });
+          return user;
+        } else {
+          throw Error("User already exists!");
+        }
+      } catch (err) {
+        return err;
+      }
+    },
+    createChannel: async (_, args) => {
+      try {
+        await ChannelMergerNode.create(args);
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+    createMessage: async (_, args) => {
+      try {
+        await MessageChannel.create({
+          ...args,
+          userId: 1
+        });
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    createTeam: async (_, args) => {
+      try {
+        await Team.create({
+          ...args,
+          owner: 1
+        });
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
   }
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,7 +1,40 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
-  type Query {
-    sayHi: String
+  type Team {
+    owner: User!
+    members: [User!]!
+    channels: [Channel!]!
   }
-`;
+  type Channel {
+    id: Int!
+    name: String!
+    public: Boolean!
+    messages: [Message!]!
+    users: [User!]!
+  }
+  type User {
+    username: String!
+    email: String!
+    password: String!
+  }
+  type Message {
+    id: Int!
+    text: String!
+    user: User!
+    channel: Channel!
+  }
+  # ----- End of Type models ----- #
+  type Query {
+    getUsers: [User!]!
+    getMessages: [Message!]
+    getUser(id: Int!): User!
+  }
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+    createMessage(channel_id: Int!, text: String!): Boolean
+    createTeam(name: String!): Boolean
+    createChannel(teamId: Int!, name: String!, public: Boolean = false): Boolean
+  }
+`
+;
